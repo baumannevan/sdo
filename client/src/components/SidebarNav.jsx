daf65518bbc2a88b58b98a7abbabea8f188e4299
baumannevan <evy42124@gmail.com>
2025-08-08 20:12:56 -0700

    sync work

--- client/src/components/SidebarNav.jsx
+++ client/src/components/SidebarNav.jsx
@@ -5,7 +5,9 @@ import "../styles/sidebar.css"; // Optional: separate sidebar styles
 export default function SidebarNav() {
   return (
     <div className="sidebar">
-      <h2>Events</h2>
+      <Link to="/home" style={{ textDecoration: "none", color: "inherit" }}>
+        <h2>Events</h2>
+      </Link>
       <h2>Required For Me</h2>
       <h2>Compliance</h2>
       <Link to="/people" style={{ textDecoration: "none", color: "inherit" }}>
